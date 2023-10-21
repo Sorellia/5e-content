bb9c3d1648ffaada312d743d631fa2edc9ce52ff
Sorellia <toothless@gmail.com>
2023-10-22 00:33:19 +0100

    Initial Commit

---
+++ setup.js
@@ -0,0 +1,6 @@
+Hooks.once("init", () => {
+	CONFIG.DND5E.featureTypes.class.subtypes.bloodRites = "Blood Rite";
+	CONFIG.DND5E.featureTypes.class.subtypes.inventorUpgrades = "Inventor Upgrade";
+	CONFIG.DND5E.featureTypes.class.subtypes.savantRunes = "Rune (Savant)";
+	CONFIG.DND5E.featureTypes.class.subtypes.scholarlyPursuits = "Scholarly Pursuit";
+});
